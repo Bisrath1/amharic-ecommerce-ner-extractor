7935547cfa2f1cab7f4e8447cf74ad93aec58277
Bisrat Haile <bisrathaile1919@gmail.com>
2025-06-24 20:17:04 +0300

    frist comparision update

---
+++ notebooks/Model_comparision.py
@@ -0,0 +1,13 @@
+from transformers import TrainingArguments, Trainer
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
+
+trainer.save_model("./model_name")
